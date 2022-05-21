4976948130d1394097ba485877aeee1e7b13a3f6
Gloria <gloghe@yahoo.it>
2022-05-21 10:31:17 +0200

    modifica testo appeso

---
+++ script2.js
@@ -0,0 +1,55 @@
+
+posts.forEach((element) => {
+
+    document.querySelector(".posts-list").innerHTML += 
+
+        `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${element.author.image} alt=${element.author.name}>                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid=${element.id}>
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+        </div>`
+
+});
+//parte esercizio 3
+
+ document.querySelectorAll('.js-like-button').forEach(item => {
+     item.addEventListener('click', event => {
+
+         document.querySelectorAll(".like-button");
+         item.classList.add("like-button--liked"); 
+        
+
+             posts.likes++
+             document.querySelectorAll("b").innerHTML= 1 ;
+        
+    
+    
+ })
+   })
+
+
